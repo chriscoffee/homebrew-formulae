2c0d7a7545f4f338d2aae9b572ee758a1e3e214e
Christopher Mills <chris.mills@enginegroup.com>
2021-03-18 13:28:43 +0000

    Add custom ncsspot formula with portaudio/covers

---
+++ ncsspot-portaudio.rb
@@ -0,0 +1,24 @@
+class Ncspot < Formula
+  desc "Cross-platform ncurses Spotify client written in Rust"
+  homepage "https://github.com/hrkfdn/ncspot"
+  url "https://github.com/hrkfdn/ncspot/archive/v0.5.0.tar.gz"
+  sha256 "c33549a4d99fa7bf6236640b112985187a7fbc1ab6689d7fe55deb14c5150191"
+  license "BSD-2-Clause"
+
+  depends_on "python@3.9" => :build
+  depends_on "rust" => :build
+
+  def install
+    ENV["COREAUDIO_SDK_PATH"] = MacOS.sdk_path_if_needed
+    system "cargo", "install",
+      "--no-default-features", "--features", "portaudio_backend,cursive/pancurses-backend,cover", *std_cargo_args
+  end
+
+  test do
+    pid = fork { exec "#{bin}/ncspot -b . -d debug.log 2>&1 >/dev/null" }
+    sleep 2
+    Process.kill "TERM", pid
+
+    assert_match '[ncspot::config] [TRACE] "./.config"', File.read("debug.log")
+  end
+end
